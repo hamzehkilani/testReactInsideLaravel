657080b0a3c416be1f1af6691994637bccd68ae1
hamzeh <1900902127h@gmail.com>
2025-02-06 04:14:46 +0300

    first commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HelloReact from './components/HelloReact.jsx';
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(<HelloReact />);
